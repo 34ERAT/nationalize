095369a063e47a11436a5e25381428c6b841727c
34ERAT <philipkanuti@gmail.com>
2025-05-27 16:52:03 +0300

    create style and java script

--- src/main.js
+++ src/main.js
@@ -8,17 +8,25 @@ const predictname = async (personName) => {
   );
   return await response.json();
 };
+
 searchBtn.addEventListener("click", async (e) => {
   const name = searchInput.value;
+  output.innerText = "";
   if (name === "") {
     output.innerText = "can not be empty";
     return;
   }
+
+  searchBtn.setAttribute("disabled", true);
   try {
-    const result = await predictname(name);
-    output.innerText = "found something intresting";
-    console.log(result);
+    const { name, country } = await predictname(name);
+    const { country_id, probability } = country[0];
+    const getCountryNames = new Intl.DisplayNames(["en"], { type: "region" });
+
+    output.innerHTML = `<span>${name}</span> is from <span>${getCountryNames.of(country_id)}</span>  with  certainty of <span>${probability * 100}</span>`;
   } catch (e) {
-    output.innerText = "something is wrong";
+    output.innerText = "something is wrong try again later";
+  } finally {
+    searchBtn.removeAttribute("disabled");
   }
 });
